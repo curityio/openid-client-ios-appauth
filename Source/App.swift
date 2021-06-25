2718307fd9db80fa89b4fb48f9023f9ce7065d24
gary.archer <gary.archer@WORKMAC.local>
2021-06-25 10:44:30 +0200

    Initial checkin of skeleton app

---
+++ Source/App.swift
@@ -0,0 +1,17 @@
+//
+//  clientApp.swift
+//  client
+//
+//  Created by gary.archer on 25/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Source/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  client
+//
+//  Created by gary.archer on 25/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
